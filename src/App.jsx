c09b7f0720a8cd7cc7dbeafcc290be564a9a5dd0
Kee0304 <kee0304@gmail.com>
2024-10-11 12:39:41 +0900

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import GptMain from "./components/GptMain"; // GptMain 컴포넌트 import
 import GptResponse from "./components/GptResponse"; // GptResponse 컴포넌트 import
 import './App.css';
@@ -8,13 +8,11 @@ import Login from "./pages/login/Login";
 function App() {
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_OAUTH_GOOGLE_CLIENTID}>
-      <Router>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/gpt-main" element={<GptMain />} /> {/* GptMain 페이지 경로 추가 */}
           <Route path="/gpt-response" element={<GptResponse />} /> {/* GptResponse 페이지 경로 추가 */}
         </Routes>
-      </Router>
     </GoogleOAuthProvider>
   );
 }
